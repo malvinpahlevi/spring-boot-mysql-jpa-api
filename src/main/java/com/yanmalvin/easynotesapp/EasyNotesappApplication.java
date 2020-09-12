de48e77fc31c60e7fe6506851b16e329465bcb46
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 00:08:11 +0700

    first project

---
+++ src/main/java/com/yanmalvin/easynotesapp/EasyNotesappApplication.java
@@ -0,0 +1,13 @@
+package com.yanmalvin.easynotesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNotesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanmalvin/easynotesapp/EasyNotesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanmalvin.easynotesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNotesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
