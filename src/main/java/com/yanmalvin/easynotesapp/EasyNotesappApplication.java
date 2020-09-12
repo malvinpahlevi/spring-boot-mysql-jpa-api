20e52c26cb07f32e61f2bf4405474b7f5ecb9996
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 00:55:31 +0700

    add anotation on main app

--- src/main/java/com/yanmalvin/easynotesapp/EasyNotesappApplication.java
+++ src/main/java/com/yanmalvin/easynotesapp/EasyNotesappApplication.java
@@ -2,8 +2,11 @@ package com.yanmalvin.easynotesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+// populated whenever create or update an entitiy (createdAt&updateAt)
+@EnableJpaAuditing
 public class EasyNotesappApplication {
 
 	public static void main(String[] args) {
