ca05aab796a0e112689d2595b6a336d6b071e353
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 00:51:21 +0700

    completed note domain

--- src/main/java/com/yanmalvin/easynotesapp/model/Note.java
+++ src/main/java/com/yanmalvin/easynotesapp/model/Note.java
@@ -7,9 +7,11 @@ package com.yanmalvin.easynotesapp.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.Table;
+import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 /**
@@ -20,9 +22,71 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 @Table(name = "notes")
 @EntityListeners(AuditingEntityListener.class)
+// jsonignore, gives values as JSon respon. *not client supply
 @JsonIgnoreProperties(value = {"createdAt", "updateAt"},
         allowGetters = true)
 public class Note implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @NotBlank
+    private String title;
     
+    @NotBlank
+    private String content;
+    
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    // Camel Case are replaced with underscores "created_at" in db
+    private Date createdAt;
+    
+    @Column(nullable = false)
+    // temporal conver date&time object<->database type 
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updateAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+    
+        
 }
