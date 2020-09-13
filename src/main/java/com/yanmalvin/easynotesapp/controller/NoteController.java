dd16f7e788865de469e0d4191dc41a77b0126b09
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 16:35:37 +0700

    add put-note-id

--- src/main/java/com/yanmalvin/easynotesapp/controller/NoteController.java
+++ src/main/java/com/yanmalvin/easynotesapp/controller/NoteController.java
@@ -27,7 +27,7 @@ public class NoteController {
     }
 
     /*
-    *  Create new note
+    * Create new note
     *
     * The @Valid annotation makes sure that the request body is valid. Remember, we had marked Note’s title and content with @NotBlank annotation in the Note model?
     * If the request body doesn’t have a title or a content, then spring will return a 400 BadRequest error to the client.
@@ -39,16 +39,32 @@ public class NoteController {
 
     /*
     *  Get single note
-    * */
+    *
+    * (ResourceNotFoundException)return a 404 Not Found error to the client
+     * */
     @GetMapping("/notes/{id}")
     public Note getNoteById(@PathVariable(value = "id") Long noteId){
         return noteRepository.findById(noteId)
                 .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
     }
 
+    /*
+    * Update Note
+    *
+    * */
+    @PutMapping("/notes/{id}")
+    public Note updateNote(@PathVariable(value = "id") Long noteId,
+                           @Valid @RequestBody Note noteDetails){
 
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
 
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
 
+        Note updateNote = noteRepository.save(note);
+        return updateNote;
 
+    }
 
 }
